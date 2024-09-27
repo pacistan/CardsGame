ee3c0e5740dfa01ce45ecdde23bcdfa2ae269d9d
Sneakysunset <andrelauzier@gmail.com>
2024-09-27 19:51:55 +0200

    DeckComponent

---
+++ Source/CardGame_ICAN_5JV_1/Deck/DeckData/DeckData.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DeckData.generated.h"
+
+UCLASS()
+class CARDGAME_ICAN_5JV_1_API UDeckData : public UDataAsset
+{
+	GENERATED_BODY()
+};
